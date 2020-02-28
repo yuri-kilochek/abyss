bba2a7586fa28a0b66f41ee980578ac1c6d8d241
Yuri Kilochek <yuri.kilochek@gmail.com>
2020-02-28 11:27:59 +0300

    Reorganized library version stuff.

--- src/version.c
+++ src/version.c
@@ -1,7 +1,5 @@
 #include <abyss/version.h>
 
-#include <abyss/included_version.h>
-
 ///////////////////////////////////////////////////////////////////////////////
 
 abyss_version_t abyss_get_linked_version(void) {
